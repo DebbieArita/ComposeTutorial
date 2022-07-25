a507cde233ddf1a8961d92c946a8c66ec2227da5
Debbie Mong'are <debbiearita96@gmail.com>
2022-07-25 18:52:34 +0300

    init compose tutorial list of messages

---
+++ app/src/main/java/com/example/composetutorial/SampleData.kt
@@ -0,0 +1,74 @@
+package com.example.composetutorial
+
+object SampleData {
+
+
+    // Sample conversation data
+    val conversationSample = listOf(
+        MainActivity.Message(
+            "Colleague",
+            "Test...Test...Test..."
+        ),
+        MainActivity.Message(
+            "Colleague",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trimMargin()
+        ),
+        MainActivity.Message(
+            "Colleague",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trimMargin()
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "Searching for alternatives to XML layouts..."
+        ),
+        MainActivity.Message(
+            "Colleague",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trimMargin()
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "It's available from API 21+ :)"
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        MainActivity.Message(
+            "Colleague",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
